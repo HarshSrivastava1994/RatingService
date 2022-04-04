c89dfd3e4b2724dee6d0ff537fbf4f2c701b2d8a
HarshSrivastava1994 <kharsh620@gmail.com>
2022-04-04 09:46:19 +0530

    Rating data service

---
+++ src/test/java/io/javabrains/Ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package io.javabrains.Ratingdataservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
